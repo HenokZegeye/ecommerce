1d6c8918c8a2ba4242702ee719b1563c64067f0d
Henok Birru <henokbirru@dyn138-121.eduroam.utu.fi>
2023-05-02 16:39:09 +0300

    Category model and an endpoint added

--- ecom/api/category/views.py
+++ ecom/api/category/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Category
+from .serializers import CategorySerializer
 
-# Create your views here.
+class CategoryViewSet(viewsets.ModelViewSet):
+    serializer_class = CategorySerializer
+    queryset = Category.objects.all().order_by('name')
